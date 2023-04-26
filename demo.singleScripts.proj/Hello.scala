66c44319addd2b395f7c4d199a02b87bca984466
Fred Bloom / hanasaki <hanasaki-github@hanaden.com>
2023-04-26 12:20:09 -0400

    refactor mv finles into demo.singleScripts.proj

---
+++ demo.singleScripts.proj/Hello.scala
@@ -0,0 +1,9 @@
+#!/usr/bin/env scala
+// scala must be installed and available on the path
+// sdkman install scala
+object Hello {
+    def main(args: Array[String]) = {
+        println("Hello, world: " + args.toList)
+    }
+}
+
